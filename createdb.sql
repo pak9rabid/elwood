b9c04e8bbef5ac630153c7aa28727a68b4dbefb7
spyke <spyke@433faabd-57f0-448e-a379-bd3f139e4d8d>
2009-12-29 17:15:16 +0000

    Added trigger to limit command history to 50 commands per user
    git-svn-id: file:///svn/trunk/elwood2@92 433faabd-57f0-448e-a379-bd3f139e4d8d

--- createdb.sql
+++ createdb.sql
@@ -7,6 +7,7 @@ CREATE TABLE settings
 	value VARCHAR (128)
 );
 
+
 DROP TABLE webterm_history;
 
 CREATE TABLE webterm_history
@@ -16,6 +17,17 @@ CREATE TABLE webterm_history
 	user VARCHAR (32) NOT NULL,
 	time TIMESTAMP NOT NULL
 );
+
+/* Create triggers */
+DROP TRIGGER webterm_history_limiter;
+
+CREATE TRIGGER webterm_history_limiter AFTER INSERT ON webterm_history
+BEGIN
+	DELETE FROM webterm_history WHERE id IN
+		(SELECT id FROM webterm_history WHERE user =
+			(SELECT user from webterm_history WHERE rowid = last_insert_rowid())
+		ORDER BY time DESC LIMIT 50 OFFSET 50);
+END;
 
 /* Initialize settings table */
 INSERT INTO settings VALUES ('IS_INITIALIZED', 'false');
