ea2bb55b86e2c71377c6c768c24d9cf466e850df
spyke <spyke@433faabd-57f0-448e-a379-bd3f139e4d8d>
2010-01-04 00:33:58 +0000

    Added firewall tables
    git-svn-id: file:///svn/trunk/elwood2@100 433faabd-57f0-448e-a379-bd3f139e4d8d

--- createdb.sql
+++ createdb.sql
@@ -13,6 +13,71 @@ CREATE TABLE webterm_history
 	time TIMESTAMP NOT NULL
 );
 
+CREATE TABLE firewall_filter_chains
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	chain VARCHAR(32) NOT NULL,
+	policy VARCHAR(32)
+);
+
+CREATE TABLE firewall_nat_chains
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	chain VARCHAR(32) NOT NULL,
+	policy VARCHAR(32)
+);
+
+CREATE TABLE firewall_filter_general
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	chain VARCHAR(32) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_filter_input
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_filter_forward_in
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_filter_forward_out
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_nat_general
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	chain VARCHAR(32) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_nat_prerouting
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE firewall_nat_postrouting
+(
+	id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+	operation VARCHAR (1) NOT NULL,
+	options VARCHAR(512) NOT NULL
+);
+
 /* Create triggers */
 CREATE TRIGGER webterm_history_limiter AFTER INSERT ON webterm_history
 BEGIN
@@ -38,3 +103,20 @@ INSERT INTO settings VALUES ('HTTPD_DIR', '/etc/elwood/httpd');
 INSERT INTO settings VALUES ('PROTOCOLS', '/etc/protocols');
 INSERT INTO settings VALUES ('WOL', '/usr/bin/wol');
 INSERT INTO settings VALUES ('FIREWALL_DIR', '/etc/elwood/firewall');
+
+/* Initialize firewall tables */
+INSERT INTO firewall_filter_chains VALUES (null, 'INPUT', 'DROP');
+INSERT INTO firewall_filter_chains VALUES (null, 'FORWARD', 'DROP');
+INSERT INTO firewall_filter_chains VALUES (null, 'OUTPUT', 'ACCEPT');
+INSERT INTO firewall_filter_chains VALUES (null, 'forward_in', null);
+INSERT INTO firewall_filter_chains VALUES (null, 'forward_out', null);
+INSERT INTO firewall_filter_general VALUES (null, 'A', 'FORWARD', '-i ' || (SELECT value FROM settings WHERE key = 'EXTIF') || ' -j forward_in');
+INSERT INTO firewall_filter_general VALUES (null, 'A', 'FORWARD', '-i ' || (SELECT value FROM settings WHERE key = 'INTIF') || ' -j forward_out');
+INSERT INTO firewall_filter_input VALUES (null, 'A', '-i ' || (SELECT value FROM settings WHERE key = 'INTIF') || ' -p tcp --dport 80 -j ACCEPT');
+INSERT INTO firewall_filter_input VALUES (null, 'A', '-i ' || (SELECT value FROM settings WHERE key = 'INTIF') || ' -p tcp --dport 22 -j ACCEPT');
+INSERT INTO firewall_filter_forward_in VALUES (null, 'A', '-m state --state RELATED,ESTABLISHED -j ACCEPT');
+INSERT INTO firewall_filter_forward_out VALUES (null, 'A', '-j ACCEPT');
+INSERT INTO firewall_nat_chains VALUES (null, 'PREROUTING' , 'ACCEPT');
+INSERT INTO firewall_nat_chains VALUES (null, 'POSTROUTING', 'ACCEPT');
+INSERT INTO firewall_nat_chains VALUES (null, 'OUTPUT', 'ACCEPT');
+INSERT INTO firewall_nat_postrouting VALUES (null, 'A', '-o ' || (SELECT value FROM settings WHERE key = 'EXTIF') || ' -j MASQUERADE');
