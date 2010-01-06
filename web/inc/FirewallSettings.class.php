eeccdeaf2f74b1bddf9a3769d202103e2357899d
spyke <spyke@433faabd-57f0-448e-a379-bd3f139e4d8d>
2010-01-06 16:51:57 +0000

    Added getChains() method
    git-svn-id: file:///svn/trunk/elwood2@109 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/inc/FirewallSettings.class.php
+++ web/inc/FirewallSettings.class.php
@@ -101,5 +101,38 @@
 
 			return $chain;
 		}
+
+		public static function getChains($tableName)
+		{
+			// Return an array of chains for the specified table
+			$chains = array();
+
+			$query = "SELECT * " .
+				 "FROM firewall_chains " .
+				 "WHERE table_name = '$tableName'";
+
+			try
+			{
+				$result = Database::executeQuery($query);
+			}
+			catch (Exception $ex)
+			{
+				throw $ex;
+			}
+
+			while (($row = sqlite_fetch_array($result, SQLITE_ASSOC)) == true)
+			{
+				$chain = new FirewallChain("firewall_chains");
+				$chain->setPrimaryKey("ID");
+				$chain->setAttribute("ID", $row['id']);
+				$chain->setAttribute("TABLE_NAME", $tableName);
+				$chain->setAttribute("CHAIN_NAME", $row['chain_name']);
+				$chain->setAttribute("POLICY", $row['policy']);
+
+				$chains[] = $chain;
+			}
+
+			return $chains;
+		}
 	}
 ?>
