6d7c4566fb746c5ec831ba3c018f8e55e648ad99
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2010-07-19 21:43:31 +0000

    Added generic exception handling for AjaxRequestHandler classes
    git-svn-id: file:///svn/trunk/elwood2@195 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/ajax/ajaxRequest.php
+++ web/ajax/ajaxRequest.php
@@ -6,35 +6,43 @@
 	$requestHandler = $_REQUEST['handler'];
 	$requestParams = $_REQUEST['parameters'];
 	
-	if (empty($requestHandler))
+	try
 	{
-		$response = new AjaxResponse("No ajax request handler specified", true);
+		if (empty($requestHandler))
+		{
+			$response = new AjaxResponse("No ajax request handler specified", true);
 		
-		echo $response->toJson();
-		exit;
+			echo $response->toJson();
+			exit;
+		}
+	
+		$requestHandlerClass = $requestHandler . "AjaxRequestHandler";
+		@include_once "$requestHandlerClass.class.php";
+	
+		if (!class_exists($requestHandlerClass))
+		{
+			$response = new AjaxResponse("Specified ajax request handler ($requestHandler) does not exist", true);
+		
+			echo $response->toJson();
+			exit;
+		}
+	
+		$requestHandlerObj = new $requestHandlerClass();
+	
+		if (!($requestHandlerObj instanceof AjaxRequestHandler))
+		{
+			$response = new AjaxResponse("Specified ajax request handler ($requestHandler) does not implement the AjaxRequestHandler interface", true);
+		
+			echo $response->toJson();
+			exit;
+		}
+	
+		$requestHandlerObj->processRequest($requestParams);
+		echo $requestHandlerObj->getResponse()->toJson();
 	}
-	
-	$requestHandlerClass = $requestHandler . "AjaxRequestHandler";
-	@include_once "$requestHandlerClass.class.php";
-	
-	if (!class_exists($requestHandlerClass))
+	catch (Exception $ex)
 	{
-		$response = new AjaxResponse("Specified ajax request handler ($requestHandler) does not exist", true);
-		
+		$response = new AjaxResponse("Exception: " . $ex->getMessage(), true);
 		echo $response->toJson();
-		exit;
 	}
-	
-	$requestHandlerObj = new $requestHandlerClass();
-	
-	if (!($requestHandlerObj instanceof AjaxRequestHandler))
-	{
-		$response = new AjaxResponse("Specified ajax request handler ($requestHandler) does not implement the AjaxRequestHandler interface", true);
-		
-		echo $response->toJson();
-		exit;
-	}
-	
-	$requestHandlerObj->processRequest($requestParams);
-	echo $requestHandlerObj->getResponse()->toJson();
 ?>
