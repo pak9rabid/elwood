c36448acfe928123d8937ece3c38cafd695fe0ad
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2010-10-31 22:54:37 +0000

    Updated status page to use the new class infrastructure
    git-svn-id: file:///svn/trunk/elwood2@231 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/status.php
+++ web/status.php
@@ -1,50 +1,48 @@
 <?php
 	require_once "accessControl.php";
 	require_once "PageElements.class.php";
-
-	require_once "status.inc";
+	require_once "NetworkSettings.class.php";
+	require_once "RouterStats.class.php";
+		
+	$networkSettings = new NetworkSettings();
 ?>
 
 <html>
 <head>
 	<title>Router Status</title>
 	<link rel="StyleSheet" type="text/css" href="css/routerstyle.css">
+	<link rel="StyleSheet" type="text/css" href="css/jquery.countdown.css">
+	<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+	<script src="js/jquery.countdown.pack.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function()
+		{
+			$("#uptime").countdown	(	{
+											since: -<?=RouterStats::getUptime()?>,
+											format: "dHMS",
+											layout: "{d<}{dn} days and {d>}{hn}:{mnn}:{snn}"
+										}
+									);
+		});
+	</script>
 </head>
 
 <body>
 	<div id="container">
 		<?=PageElements::titleOut("Status")?>
 		<?=PageElements::navigationOut()?>
-		<div id="content">
-		<?php
-			# Get current router status
-			$routerStatus = new routerStatus;
-
-			# Display current router status
-echo <<<END
-			<table class="status-table">
-				<tr><th>WAN IP Address:</th><td>$routerStatus->wanIP</td></tr>
-				<tr><th>LAN IP Address:</th><td>$routerStatus->lanIP</td></tr>
-				<tr><th>&nbsp</th><td>&nbsp</td></tr>
-END;
-			foreach ($routerStatus->dnsList as $key => $value)
-			{
-				$index = $key + 1;
-				echo "<tr><th>Nameserver $index:</th><td>$value</td></tr>";
-			}
-echo <<<END
-				<tr><th>&nbsp</th><td>&nbsp</td></tr>
-				<tr><th>Uptime:</th><td>$routerStatus->uptime</td></tr>
-				<tr><th>Active Interfaces:</th><td>
-END;
-			foreach ($routerStatus->activeIfacesList as $value)
-				echo "$value ";
-
-			echo "</td></tr>";
-echo <<<END
+		<div id="content">	
+			<table class="status-table" style="width: 60%;">
+				<tr><th>WAN IP Address:</th><td><?=$networkSettings->getWanInterface()->getIp()?></td></tr>
+				<tr><th>LAN IP Address:</th><td><?=$networkSettings->getLanInterface()->getIp()?></td></tr>
+				<tr><th>&nbsp;</th><td>&nbsp;</td></tr>
+				
+<?php foreach ($networkSettings->getNameservers() as $key => $nameserver) { ?>
+				<tr><th>Nameserver <?=$key + 1?>:</th><td><?=$nameserver?></td></tr>
+<?php } ?>
+				<tr><th>&nbsp;</th><td>&nbsp;</td></tr>
+				<tr><th>Uptime:</th><td id="uptime"></td></tr>
 			</table>
-END;
-	?>
 		</div>
 	</div>
 </body>
