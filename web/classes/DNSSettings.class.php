7d93ced9108e6abf0c486ddf370adc2a9516874d
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2010-11-04 15:43:55 +0000

    Added validation checking to the setNameservers() method
    git-svn-id: file:///svn/trunk/elwood2@234 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/classes/DNSSettings.class.php
+++ web/classes/DNSSettings.class.php
@@ -1,5 +1,6 @@
 <?php
 	require_once "FileUtils.class.php";
+	require_once "NetUtils.class.php";
 	
 	class DNSSettings
 	{
@@ -27,6 +28,12 @@
 		
 		public function setNameservers(array $nameservers)
 		{
+			foreach ($nameservers as $nameserver)
+			{
+				if (!NetUtils::isValidIp($nameserver))
+					throw new Exception("Invalid nameserver specified");
+			}
+			
 			$this->nameservers = $nameservers;
 		}
 		
