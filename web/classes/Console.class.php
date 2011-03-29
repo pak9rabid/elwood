bc1bd0ea7b7d502f2ef53be5e69ff5f3ac53e617
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2011-03-29 18:33:25 +0000

    Added option to ignore errors when executing a shell command (defaulted to not ignore)
    git-svn-id: file:///svn/trunk/elwood2@338 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/classes/Console.class.php
+++ web/classes/Console.class.php
@@ -1,12 +1,12 @@
 <?php
 	class Console
 	{
-		public static function execute($command)
+		public static function execute($command, $ignoreErrors = false)
 		{
 			$output = array();
 			$line = exec($command, $output, $exitCode);
 
-			if ($exitCode != 0)
+			if (!$ignoreErrors && $exitCode != 0)
 				throw new Exception("Error encountered when running command: $command");
 			
 			return $output;
