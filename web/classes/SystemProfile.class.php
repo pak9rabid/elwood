c6c49fbf2961c949a8a523e862a58fd4413c00f4
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2011-07-04 14:15:34 +0000

    Added getAvailableProfiles() method
    git-svn-id: file:///svn/trunk/elwood2@350 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/classes/SystemProfile.class.php
+++ web/classes/SystemProfile.class.php
@@ -8,5 +8,15 @@
 			return json_decode(file_get_contents(RouterSettings::getSettingValue("ELWOOD_CFG_DIR") .
 				"/profiles/" . RouterSettings::getSettingValue("SYSTEM_PROFILE")));
 		}
+		
+		public static function getAvailableProfiles()
+		{
+			return array_filter(scandir(RouterSettings::getSettingValue("ELWOOD_CFG_DIR") . "/profiles"), array("self", "scandirFilter"));
+		}
+		
+		private static function scandirFilter($filename)
+		{
+			return $filename != "." && $filename != "..";
+		}
 	}
 ?>
