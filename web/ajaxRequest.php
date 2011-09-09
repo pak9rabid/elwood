7c3ae4cd939b949dcdd3386fd75f1f3a4fc07abe
root <root@433faabd-57f0-448e-a379-bd3f139e4d8d>
2011-09-09 20:43:00 +0000

    Removed old, deprecated code
    git-svn-id: file:///svn/trunk/elwood2@388 433faabd-57f0-448e-a379-bd3f139e4d8d

--- web/ajaxRequest.php
+++ web/ajaxRequest.php
@@ -46,12 +46,6 @@
 			exit;
 		}
 	
-		/* OLD
-		$requestHandlerObj->processRequest($requestParams);
-		echo $requestHandlerObj->getResponse()->toJson();
-		*/
-		
-		/* NEW */
 		$response = $requestHandlerObj->processRequest($requestParams);
 		
 		if (!($response instanceof AjaxResponse))
